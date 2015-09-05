d255ca427346f22f424a0381d79636b6529f4dcf
Proudmoor <zpf238@gmail.com>
2015-09-05 17:14:15 +0800

    with PagerViewer

--- Dota2LastMacth/app/src/main/java/com/zpf416/dota2lastmacth/MatchActivity.java
+++ Dota2LastMacth/app/src/main/java/com/zpf416/dota2lastmacth/MatchActivity.java
@@ -3,12 +3,17 @@ package com.zpf416.dota2lastmacth;
 
 import android.app.Fragment;
 
+import java.util.UUID;
+
 /**
  * Created by pengfei on 15-9-2.
  */
 public class MatchActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment(){
-        return new MatchFragment();
+        UUID matchId = (UUID)getIntent()
+                .getSerializableExtra(MatchFragment.EXTRA_MATCH_ID);
+
+        return MatchFragment.newInstance(matchId);
     }
 }
